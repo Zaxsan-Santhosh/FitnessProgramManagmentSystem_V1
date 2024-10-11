88949e1e4870492a937cb303fcc1117667d8b23e
UT01198 <UT01198@UNICOMTIC33>
2024-10-11 10:50:58 +0530

    Add project files.

---
+++ FitnessProgram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagmentSystem
+{
+    internal class FitnessProgram
+    {
+        private string fitnessProgramId { get; set; }
+        private string title { get; set; }
+        private string duration { get; set; }
+        private string price { get; set; }
+
+
+        public FitnessProgram(string ProgramId , string Title , string Duration , string Price)
+        {
+            this.fitnessProgramId = ProgramId;
+            this.title = Title;
+            this.duration = Duration;
+                this.price = Price;
+
+        }
+
+
+        public override string ToString()
+        {
+           return($"fitnessProgramId: {fitnessProgramId}, Title: {title}, Duration: {duration}, Price: {price}");
+           
+        }
+
+
+    }
+
+
+    
+}
---
+++ FitnessProgramManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagmentSystem
+{
+    internal class FitnessProgramManager
+    {
+        public List<FitnessProgram> manager = new List<FitnessProgram>();
+
+
+        public void CreateFitnessProgram(FitnessProgram program)
+        {
+          manager.Add(program);
+          return;
+
+        }
+
+        public void ReadFitnessProgram()
+        {
+            foreach (var Program in manager) 
+            {
+                Console.WriteLine(Program);
+            }
+        }
+
+
+        public void UpdateFitnessProgram(string Proid, string _title ,string _duration,string _price)
+        {
+            if (Proid != null) 
+            {
+                var updatedId = manager.Find(p => p. );
+            }
+
+            else
+            {
+                Console.WriteLine("Check The Id");
+            }
+        }
+
+
+
+        public void DeleteFitnessProgram(int id)
+        {
+            if ( id != null )
+            {
+                var delId = manager.Find(p => p.);
+            }
+
+            else
+            {
+                Console.WriteLine("Check The Id")
+            }
+        }
+    }
+
+}
